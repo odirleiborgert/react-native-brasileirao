2309d04155766696de12f66a4f7129f844106a16
Odirlei Borgert <odirleiborgert@gmail.com>
2020-02-09 16:45:24 -0300

    Working prototype

--- src/components/NavGuess/styles.js
+++ src/components/NavGuess/styles.js
@@ -6,6 +6,8 @@ export const Item = styled.View`
     justify-content: center;
     align-items: flex-start;
     margin-right: 10px;
+    height: 50px;
+    padding-bottom: 10px;
 `
 
 export const ScrollView = styled.ScrollView.attrs({
